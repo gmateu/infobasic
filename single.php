6b3e7cd96c60cd097ef4d25fe95b4828723fa6bb
Your Name <you@example.com>
2021-01-07 08:27:11 +0100

    generat single.php per veure posts simples

--- single.php
+++ single.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi página web</title>
+    <title>Entrada individual</title>
 </head>
 <body>
-<h1>Bienvenidos a mi web</h1>
+<h1>Entrada indivdual</h1>
     <?php
         while(have_posts()){
             the_post();
             ?>
             <h2><a href="<?=the_permalink()?>"><?=the_title()?></a></h2>
-            <p><?=the_excerpt()?></p>
+            <p><?=the_content()?></p>
         <?php
         }
     ?>
