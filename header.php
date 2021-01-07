fcd2863823e5fcac5e484c2c0e21ef1df89abfb4
Your Name <you@example.com>
2021-01-07 17:13:48 +0100

    creado functions.php y añadido css de terceros

--- header.php
+++ header.php
@@ -3,8 +3,6 @@
 <head>
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <?php wp_head();?>
     <title>Bienvenidos a mi weg</title>
 </head>
