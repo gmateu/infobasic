7411fec21d5007ca20bd1905ccc7e25d87462716
Your Name <you@example.com>
2021-01-25 15:09:16 +0100

    registrar menús

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head();?>
-    <title>Bienvenidos a mi weg</title>
+    <title>Bienvenidos a mi web</title>
 </head>
 <body <?php body_class( )?>>
 
