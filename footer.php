edb4fc5fb4db7e1c1eca3be1f2c91bdee016c83d
guillem mateu <gmateu@esliceu.net>
2021-02-02 08:57:49 +0100

    como crear menus de enlaces

--- footer.php
+++ footer.php
@@ -1,6 +1,24 @@
 
 <footer>
-    <h3>Aquí irá nuestro pie de página</h3>
+    <div class="row">
+        <div class="col-6">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="col-3">
+        <?php
+            wp_nav_menu( array(
+                'theme_location'    => 'infobasic_links_menu',
+            ) );
+        ?>
+        </div>
+        <div class="col-3">
+        <?php
+            wp_nav_menu( array(
+                'theme_location'    => 'infobasic_negoci_menu',
+            ) );
+        ?>
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer();?>
--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@ function infobasic_config(){
     register_nav_menus(
         array(
             'infobasic_main_menu' => 'Info Basic menú principal',
+            'infobasic_links_menu' => 'Info Basic menú links',
+            'infobasic_negoci_menu' => 'Info Basic menú negoci',
         )
     );
 
@@ -16,5 +18,6 @@ add_action( 'after_setup_theme', 'infobasic_config', 0 );
 function infobasic_scripts(){
     wp_enqueue_script( "bootstrap_js", get_theme_file_uri("inc/js/bootstrap.min.js"), array("jquery"),"4.5", true );
     wp_enqueue_style( "bootstrap_css", get_theme_file_uri("inc/css/bootstrap.min.css"),array(), "4.5","all" );
+    wp_enqueue_style( "custom_css", get_theme_file_uri("css/custom.css"),array(),  filemtime(get_template_directory().'/css/custom.css'),"all" );
 }
 add_action( 'wp_enqueue_scripts', 'infobasic_scripts');
