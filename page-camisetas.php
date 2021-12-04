bf3dd42b6d3f4a1a35371c781f8146c7cef30747
Your Name <you@example.com>
2021-12-04 19:48:22 +0100

    personalizadas camisetas

--- page-camisetas.php
+++ page-camisetas.php
@@ -4,6 +4,36 @@
    <div class="jumbotron jumbo-camisetas">
        <h1>Compra nuestras camisetas</h1>
    </div>
+   <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card-img-top">
+                    <img src="<?=get_theme_file_uri("inc/img/camiseta.jpg")?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>    
+            </div>
+            <div class="col-md-3">
+            <div class="card-img-top">
+                    <img src="<?=get_theme_file_uri("inc/img/camiseta.jpg")?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>     
+            </div>
+            <div class="col-md-3">
+            <div class="card-img-top">
+                    <img src="<?=get_theme_file_uri("inc/img/camiseta.jpg")?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>     
+            </div>
+            <div class="col-md-3">
+            <div class="card-img-top">
+                    <img src="<?=get_theme_file_uri("inc/img/camiseta.jpg")?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>     
+            </div>
+    
+        </div>
+    </div>
+   
 
 <?php
     get_footer( );
