630edce97930e15cc8aefd381d7331adcd6b887e
Your Name <you@example.com>
2021-12-04 19:38:01 +0100

    personalizada page-tienda.php

--- page-tienda.php
+++ page-tienda.php
@@ -5,6 +5,31 @@
         <h1>Compra tu ropa online</h1>
     </div>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <img src="<?=get_theme_file_uri("inc/img/samarretes.jpg")?>" alt="" srcset="">
+                    <div class="card-body">
+                        <h5>Camisetas a medida</h5>
+                        <a href="<?=site_url( "camisetas")?>" class="btn btn-primary">Ir...</a>
+                    </div>
+                </div>    
+            </div>
+            <div class="col-md-6">
+            <div class="card">
+                    <img src="<?=get_theme_file_uri("inc/img/jeans.jpg")?>" alt="" srcset="">
+                    <div class="card-body">
+                        <h5>Tejanos flexibles</h5>
+                        <a href="<?=site_url( "tejanos")?>" class="btn btn-primary">Ir...</a>
+                    </div>
+                </div>     
+            </div>
+    
+        </div>
+    </div>
+
+
 <?php
     get_footer( );
 ?>
