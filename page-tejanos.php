7402b4cedfaf89549d53fd8001f64c9358a2a714
Your Name <you@example.com>
2021-12-04 19:55:31 +0100

    personalizada tejanos

--- page-tejanos.php
+++ page-tejanos.php
@@ -4,7 +4,35 @@
     <div class="jumbotron jumbo-jeans">
         <h1>Prueba nuestro jeans flexibles</h1>
     </div>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card-img-top">
+                    <img src="<?=get_theme_file_uri("inc/img/jeans1.jpg")?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>    
+            </div>
+            <div class="col-md-3">
+            <div class="card-img-top">
+                    <img src="<?=get_theme_file_uri("inc/img/jeans1.jpg")?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>     
+            </div>
+            <div class="col-md-3">
+            <div class="card-img-top">
+                    <img src="<?=get_theme_file_uri("inc/img/jeans1.jpg")?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>     
+            </div>
+            <div class="col-md-3">
+            <div class="card-img-top">
+                    <img src="<?=get_theme_file_uri("inc/img/jeans1.jpg")?>" alt="" srcset="">
+                    <p>1981 - 19,90 €</p>
+                </div>     
+            </div>
+    
+        </div>
+    </div>
     
 <?php
     get_footer( );
